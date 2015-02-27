a16d57d10970f01d4c7be483f6c7d89bde20f47d
sunny <indiasuny000@gmail.com>
2015-02-27 16:08:54 +0530

    Improved search

--- app/js/bloodbank.js
+++ app/js/bloodbank.js
@@ -1,12 +1,28 @@
 angular.module('bloodbank', [])
   .controller('searchCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.results = [];
+    $scope.groups = [
+      { label: 'Any group', value: 'any'},
+      { label: 'A+', value: '`A+`'},
+      { label: 'A-', value: '`A-`'},
+      { label: 'B+', value: '`B+`'},
+      { label: 'B-', value: '`B-`'},
+      { label: 'AB+', value: '`AB+`'},
+      { label: 'O+', value: '`O+`'}
+    ];
+    $scope.selectedGroup = $scope.groups[0];
 
     $scope.search = function () {
       var word = $scope.keyword;
-      console.log('searching for ' + word);
-      var query = '/search/' + word;
-      console.log(query);
+      var query = '/search/';
+      console.log('word is', word);
+      if (! _.isUndefined(word)) {
+        query += word + '/';
+      }
+      if ($scope.selectedGroup.value != 'any') {
+        query += $scope.selectedGroup.value;
+      }
+      console.log('querying ' + query);
       $http.get(query)
         .success(function (data, status, headers, config) {
           $scope.results = data;
--- server.js
+++ server.js
@@ -11,7 +11,20 @@ app.set('json spaces', 2);
 router.get('/search/:keyword', function (req, res) {
   console.log('Got req');
   var keyword = req.params.keyword;
+  console.log('query string:', keyword);
   db.search(keyword).then(function (result) {
+    console.log('result:', result);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+  })
+});
+
+router.get('/search/:name/:group', function (req, res) {
+  console.log('got find req');
+  var query = "value.Name:" + req.params.name + " AND " +
+              "value.Group:" + req.params.group;
+  console.log('query string:', query);
+  db.search(query).then(function (result) {
     console.log('result:', result);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(result));
